a05299d11b964d98d04b6334b2661a02a772cd38
dcx2010 <275814977@qq.com>
2020-10-11 21:52:55 +0800

    自定义类 Student

---
+++ Student.java
@@ -0,0 +1,17 @@
+package learnJavaCode;
+
+public class Student {
+
+    //成员变量
+    String name;
+    int age;
+
+
+    //成员方法
+    public void eat(){
+        System.out.println("吃饭");
+
+    }
+
+
+}
