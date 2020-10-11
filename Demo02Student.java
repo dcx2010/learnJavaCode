ec49d7883233b7b497171997f8a7aa77a88b30d6
dcx2010 <275814977@qq.com>
2020-10-11 21:52:15 +0800

    自建类Student的测试应用

---
+++ Demo02Student.java
@@ -0,0 +1,15 @@
+package learnJavaCode;
+
+public class Demo02Student {
+    public static void main(String[] args) {
+
+        Student stu = new Student();
+        stu.eat();
+        stu.name = "赵丽颖";
+        System.out.println(stu.name);
+
+    }
+
+
+
+}
