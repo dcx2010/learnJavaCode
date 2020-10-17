da7ac5cc4c4204811afd39f4fc1cb5a823e97885
dcx2010 <275814977@qq.com>
2020-10-17 22:46:00 +0800

    String 构造方法，添加几个练习

--- StringMemberMethod.java
+++ StringMemberMethod.java
@@ -7,16 +7,41 @@ public class StringMemberMethod {
         String str1 = "ajdlsajflsdjflka";
         String str2 = "ABC";
 
+        //length 用法举例
         System.out.println(str1.length());
 
+        // concat 用法举例
         String str3 = str1.concat(str2);
         System.out.println(str3);
 
+        //charAt 用法举例
         System.out.println(str1.charAt(2));
 
+        //indexOf 用法举例
         System.out.println(str1.indexOf('l'));
 
         System.out.println(str1.indexOf("lsa"));
+
+        //substring 用法举例
+        String str4 = "Hello,World";
+        String str5 = str4.substring(3,6);
+        System.out.println(str5);
+
+
+        //toCharArray getBytes replace 用法举例、
+        String str6 = "How do you do";
+        char[] ch1 = str6.toCharArray();
+        System.out.println(ch1);
+        System.out.println(ch1[1]);
+
+        byte[] by = str6.getBytes();
+        System.out.println(by);
+        System.out.println(by[1]);
+
+        String str7 = str6.replace("o", "@");
+        System.out.println(str7);
+
+
 
 
 
