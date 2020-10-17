79f95ca686f360a9a3d04b46b2a0836a1134769f
dcx2010 <275814977@qq.com>
2020-10-17 22:14:44 +0800

    String 类的一些成员方法的练习

--- StringEquals.java
+++ StringEquals.java
@@ -1,2 +1,22 @@
-package learnJavaCode;public class StringEquals {
+package learnJavaCode;
+
+public class StringEquals {
+    public static void main(String[] args) {
+        String a = "abc";
+        String b = "Abc";
+        char[] d = {'a','b','c'};
+
+        String c = new String(d);
+
+        System.out.println(a.equals(b));
+        System.out.println(a.equals(c));
+
+        System.out.println(a==c);
+
+
+
+    }
+
+
+
 }
--- StringMemberMethod.java
+++ StringMemberMethod.java
@@ -1,2 +1,25 @@
-package learnJavaCode;public class StringMemberMethod {
+package learnJavaCode;
+
+import java.util.SplittableRandom;
+
+public class StringMemberMethod {
+    public static void main(String[] args) {
+        String str1 = "ajdlsajflsdjflka";
+        String str2 = "ABC";
+
+        System.out.println(str1.length());
+
+        String str3 = str1.concat(str2);
+        System.out.println(str3);
+
+        System.out.println(str1.charAt(2));
+
+        System.out.println(str1.indexOf('l'));
+
+        System.out.println(str1.indexOf("lsa"));
+
+
+
+    }
+
 }
