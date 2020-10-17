5e27a5dbb843b7d9db0fb18d7ac5ae43480ab29c
dcx2010 <275814977@qq.com>
2020-10-17 22:13:39 +0800

    学生类创建练习

--- Student3.java
+++ Student3.java
@@ -1,2 +1,30 @@
-package learnJavaCode;public class Student3 {
+package learnJavaCode;
+
+public class Student3 {
+    private String name;
+    private int age;
+
+    public Student3() {
+    }
+
+    public Student3(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
